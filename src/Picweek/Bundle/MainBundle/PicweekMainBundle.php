baa2ea985c3337ccd0da290995d8e13419671de9
giboow <philippe.gibert@gmail.com>
2012-06-13 14:44:50 +0200

    update doc

--- src/Picweek/Bundle/MainBundle/PicweekMainBundle.php
+++ src/Picweek/Bundle/MainBundle/PicweekMainBundle.php
@@ -4,6 +4,12 @@ namespace Picweek\Bundle\MainBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
+/**
+ * Picweek main bundle
+ * 
+ * @author Philippe Gibert <philippe.gibert@gmail.com>
+ * @since 0.1
+ */
 class PicweekMainBundle extends Bundle
 {
 }
