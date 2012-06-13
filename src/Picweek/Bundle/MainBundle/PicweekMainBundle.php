96884c59e14e3e5bb3170a31642c8d591502155d
giboow <philippe.gibert@gmail.com>
2012-06-13 15:14:40 +0200

    correct doc

--- src/Picweek/Bundle/MainBundle/PicweekMainBundle.php
+++ src/Picweek/Bundle/MainBundle/PicweekMainBundle.php
@@ -8,7 +8,7 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
  * Picweek main bundle
  * 
  * @author Philippe Gibert <philippe.gibert@gmail.com>
- * @since 0.1
+ * @since  0.1
  */
 class PicweekMainBundle extends Bundle
 {
