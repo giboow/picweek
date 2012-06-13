953e590168746e5c5d72e875c907d96ab5d10c9b
giboow <philippe.gibert@gmail.com>
2012-06-13 16:44:19 +0200

    phpcs

--- src/Picweek/Bundle/MainBundle/Tests/Controller/DefaultControllerTest.php
+++ src/Picweek/Bundle/MainBundle/Tests/Controller/DefaultControllerTest.php
@@ -6,15 +6,16 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 /**
  * Picweek/Bundle/MainBundle/DefaultController testcase
- * 
+ *
  * @author Philippe Gibert <philippe.gibert@gmail.com>
  * @since  0.1
  */
 class DefaultControllerTest extends WebTestCase
 {
-	/**
-	 * Test Index action
-	 */
+
+    /**
+     * Test Index action
+     */
     public function testIndex()
     {
         $client = static::createClient();
