2711a652013e07f36c8dac9d55098259e06a70f0
giboow <philippe.gibert@gmail.com>
2012-08-10 12:38:44 +0200

    add User controller, routing and join form

---
+++ src/Picweek/Bundle/MainBundle/Controller/UserController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Picweek\Bundle\MainBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+/**
+ * User controller
+ *
+ * @author Philippe Gibert <philippe.gibert@gmail.com>
+ * @since  0.1
+ */
+class UserController extends Controller
+{
+
+    /**
+     * Render main page
+     *
+     * @Route("/join")
+     * @Template()
+     * @return array
+     */
+    public function joinAction()
+    {
+        return array();
+    }
+}
