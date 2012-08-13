28dbcf501a3d8804076a607a0aefdfc8153540f5
giboow <philippe.gibert@gmail.com>
2012-08-13 08:40:53 +0200

    add user join overlay

--- src/Picweek/Bundle/MainBundle/Controller/UserController.php
+++ src/Picweek/Bundle/MainBundle/Controller/UserController.php
@@ -17,12 +17,17 @@ class UserController extends Controller
     /**
      * Render main page
      *
-     * @Route("/join")
+     * @Route("/join", name="_user_join")
      * @Template()
      * @return array
      */
     public function joinAction()
     {
+        $request = $this->getRequest();
+        if ($request->isXmlHttpRequest()) {
+            return $this->render('PicweekMainBundle:User:join.ajax.twig', array());
+        }
+
         return array();
     }
 }
